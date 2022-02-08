6d968820f92aa396fe8fd29f15f6c38b29cfcfe0
amarkaryan <amarkaryan@ozon.ru>
2022-02-08 23:56:46 +0300

    v1 done

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/artemmarkaryan/airport_metronome
+
+go 1.17
+
+require github.com/benbjohnson/clock v1.3.0
---
+++ main.go
@@ -0,0 +1,101 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"strconv"
+	"sync"
+	"time"
+)
+
+type C struct {
+	sync.Mutex
+	t              uint64
+	secondDuration float64
+}
+
+func (c *C) start() {
+	for {
+		time.Sleep(time.Millisecond * time.Duration(c.secondDuration*1000))
+		c.Mutex.Lock()
+		c.t += 1
+		c.Mutex.Unlock()
+	}
+}
+
+func (c *C) getTime(w http.ResponseWriter, _ *http.Request) {
+	var t uint64
+	{
+		c.Mutex.Lock()
+		t = c.t
+		c.Mutex.Unlock()
+	}
+
+	_, _ = fmt.Fprintf(w, "%d", t)
+}
+
+func (c *C) setSpeed(w http.ResponseWriter, req *http.Request) {
+	if req.Method != http.MethodGet {
+		writeError(w, http.StatusBadRequest, "wrong method, use get")
+		return
+	}
+
+	var q []string
+	var ok bool
+	if q, ok = req.URL.Query()["speed"]; !ok {
+		writeError(w, http.StatusBadRequest, "speed parameter not found")
+		return
+	}
+
+	if len(q) != 1 {
+		writeError(w, http.StatusBadRequest, "speed parameter must be single")
+		return
+	}
+
+	speed, err := strconv.ParseInt(q[0], 10, 64)
+	if err != nil {
+		writeError(w, http.StatusBadRequest, fmt.Sprintf("error parsing speed: %s", err.Error()))
+		return
+	}
+
+	c.Lock()
+	c.secondDuration = 1.0 / float64(speed)
+	c.Unlock()
+
+	w.WriteHeader(http.StatusOK)
+	return
+}
+
+func main() {
+	c := new(C)
+	c.secondDuration = 1
+	go c.start()
+
+	http.HandleFunc("/time", c.getTime)
+	http.HandleFunc("/setSpeed", c.setSpeed)
+
+	http.ListenAndServe(":8090", nil)
+}
+
+func writeError(w http.ResponseWriter, status int, description string) {
+	b, err := json.Marshal(
+		struct {
+			Msg string `json:"msg"`
+		}{
+			Msg: description,
+		},
+	)
+	if err != nil {
+		log.Printf("error converting error to json: %s", err.Error())
+		return
+	}
+
+	w.WriteHeader(status)
+	_, err = w.Write(b)
+	if err != nil {
+		log.Printf("error writing response: %s", err.Error())
+		return
+	}
+}
